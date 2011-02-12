03c2fa9aabdba2dea3a42b3dd9e65b13cacdc1ae
mig5 <mig@mig5.net>
2011-02-12 15:41:22 +1100

    let's not depend on ConfigParse in the fabfile if we already do in the main code, and use fab function arguments to pass relevant values to it.

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,6 @@
 from fabric.api import *
-import ConfigParser
 import os
 
-# Fetch some values from the config file
-config = ConfigParser.RawConfigParser()
-config.read('config/madelon.ini')
 
 # Function to place private key and known_hosts on the remote host
 # so that we can git clone a private repo via SSH successfully
@@ -13,9 +9,9 @@ def deploy_key():
 	put('known_hosts', '/root/.ssh/known_hosts')
 
 # Install and run the puppet manifests on the remote host
-def deploy_puppet():
-        run('git clone %s /opt/puppet' % config.get('Git', 'repo'), pty=True)
-	if config.getboolean('Puppet', 'simulate'):
+def deploy_puppet(repo, simulate='false'):
+        run('git clone %s /opt/puppet' % repo, pty=True)
+	if simulate == 'true':
 		run('puppet --verbose /opt/puppet/puppet/init.pp --modulepath=/opt/puppet/puppet/modules --noop', pty=True)
 	else:
 	        run('puppet --verbose /opt/puppet/puppet/init.pp --modulepath=/opt/puppet/puppet/modules', pty=True)
